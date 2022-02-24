6881b5f490cfcf7af65a33edc13452bee3b4f6c9
Brian Lin <brnlin@ucsc.edu>
2022-02-24 14:10:50 -0800

    Push before rebase

--- routes/items/index.js
+++ routes/items/index.js
@@ -1,20 +1,19 @@
 'use strict'
 
-const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb')
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-
-    const client = new DynamoDBClient({ region: "us-east-1" });
+    const client = new DynamoDBClient({ region: 'us-east-1' })
 
     const params = {
-      TableName : "items",
-    };
+      TableName: 'items'
+    }
 
-    const command = new ScanCommand(params);
+    const command = new ScanCommand(params)
 
-    const response = await client.send(command);
-    
-    return response.Items;
+    const response = await client.send(command)
+
+    return response.Items
   })
 }
--- routes/mqtt/index.js
+++ routes/mqtt/index.js
@@ -3,22 +3,20 @@
 const {dynamoHW, dynamoUI} = require('./api_calls')
 
 module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+    let itemID = 'bussin bussin'
 
-    fastify.get('/', async function (request, reply) {
+    const params = {
+      TableName: 'items',
+      Key: {
+        item_id: itemID
+      }
+    }
 
-        let itemID = 'bussin bussin'
-        
-        const params = {
-            TableName : 'items',
-            Key: {
-                item_id: itemID
-            }
-        }
-  
-        const hwData = await dynamoHW(this.dynamo, params)
-        console.log(hwData)
-        const uiData = await dynamoUI(this.dynamo, params)
+    const hwData = await dynamoHW(this.dynamo, params)
+    console.log(hwData)
+    const uiData = await dynamoUI(this.dynamo, params)
 
-        reply.code(200).send(uiData)
+    reply.code(200).send(uiData)
   })
 }
