4805e17e1e814c408b0258f2eb9fc17284741f6b
Brian Lin <brnlin@ucsc.edu>
2022-02-24 16:41:39 -0800

    Updated endClient function to custommqqttclient class

--- plugins/fastify-mclient.js
+++ plugins/fastify-mclient.js
@@ -4,16 +4,21 @@ const fp = require('fastify-plugin')
 const mqtt = require('mqtt')
 const fastJSONStringify = require('fast-json-stringify')
 
-class customMqttClient {
-    constructor(mqttClient) {
-        this.mqttClient = mqttClient
-    }
+class CustomMqttClient {
+  MqttCustom (mqttClient) {
+    this.mqttClient = mqttClient
+  }
 
-    async submitOrder(orderID, machineID, orderList) {
-        const order = {"machineID":machineID, "orderID":orderID, "orderList":orderList}
-        const orderJSON = JSON.parse(fastJSONStringify(order))
-        this.mqttClient.publish(machineID, orderJSON)
-    }
+  async submitOrder (orderID, machineID, orderList) {
+    const order = {'machineID': machineID, 'orderID': orderID, 'orderList': orderList}
+    const orderJSON = JSON.parse(fastJSONStringify(order))
+    this.mqttClient.publish(machineID, orderJSON)
+  }
+
+  endClient (err) {
+    console.error(err)
+    this.mqttClient.end()
+  }
 }
 
 function fastifyCustomMQTTClient (fastify, options, next) {
@@ -28,14 +33,13 @@ function fastifyCustomMQTTClient (fastify, options, next) {
 
   let mqttClient = mqtt.connect(host, { username: username, password: password })
 
-  mqttClient.on('error', (err) => {
-    console.error(err)
-    mqttClient.end()
+  var customClient = new CustomMqttClient(mqttClient)
+
+  customClient.mqttClient.on('error', (err) => {
+    customClient.endClient(err)
   })
 
-  fastify.addHook('onClose', () => mqttClient.end())
-
-  var customClient = new customMqttClient(mqttClient)
+  fastify.addHook('onClose', () => customClient.endClient())
 
   if (!fastify.mqtt) {
     fastify.decorate('customMqttClient', customClient)
---
+++ test/plugins/fastify-mclient.test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { test } = require('tap')
+const Fastify = require('fastify')
+const customMqtt = require('../../plugins/fastify-mclient')
+
+test('support works standalone', async (t) => {
+  const fastify = Fastify()
+  fastify.register(customMqtt, {
+    host: 'http://ec2-3-87-77-241.compute-1.amazonaws.com:1884',
+    username: 'lenatest',
+    password: 'password'
+  })
+  await fastify.ready()
+  t.ok(fastify.customMqtt)
+
+  fastify.customMqtt.end(true)
+})
