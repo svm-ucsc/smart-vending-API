a9ef398fbc9cd5f685092cd9e5ee7d63cc56ee06
Lena <lena.hicksonlong@gmail.com>
2022-03-03 17:29:41 -0800

    allow COR Method on /item

--- routes/item/index.js
+++ routes/item/index.js
@@ -35,7 +35,7 @@ module.exports = async function (fastify, opts) {
       }
 
       const response = await this.dynamo.scan(params)
-      reply.code(200).header('Access-Control-Allow-Origin', '*').send(response.Items)
+      reply.code(200).header('Access-Control-Allow-Origin', '*').header("Access-Control-Allow-Methods", "GET").send(response.Items)
     }
   })
 }
