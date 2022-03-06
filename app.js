b7963a2694c1bc90aa38944f7a11abf3e0d62617
Lena <lena.hicksonlong@gmail.com>
2022-03-05 17:22:18 -0800

    Add swagger API documentation

--- app.js
+++ app.js
@@ -14,6 +14,33 @@ module.exports = async function (fastify, opts) {
     password: 'password'
   })
 
+  fastify.register(require('fastify-swagger'), {
+    routePrefix: '/documentation',
+    swagger: {
+      info: {
+        title: 'API documentation',
+        version: '0.1.0'
+      },
+      port: '3000',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'routes', description: 'All endpoints' }
+      ]
+    },
+    uiConfig: {
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) { next() },
+      preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    exposeRoute: true
+  })
+
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
--- routes/machine/index.js
+++ routes/machine/index.js
@@ -1,11 +1,40 @@
 'use strict'
 
 const schema = {
+  description: 'Gets information on a vending machine, including its stock.',
+  tags: ['routes'],
+  summary: 'Gets machine information',
   querystring: {
     type: 'object',
     properties: {
-      mids: { type: ['string', 'array'] },
-      fields: { type: ['string', 'array'] }
+      mids: {
+        type: ['string', 'array'],
+        description: 'Machine IDs. Leave blank to get all machines.'
+      },
+      fields: {
+        type: ['string', 'array'],
+        description: 'Desired return fields. Leave blank to get all fields.'
+      }
+    }
+  },
+  response: {
+    200: {
+      description: 'Succsesfully got the machine data requested. The properties returned depends on the inputed "fields" paramater. The response is always an array, even if there is one or zero items returned.',
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          machine_id: { type: 'string' },
+          active: { type: 'boolean' },
+          location: { type: 'string' },
+          stock: {
+            type: 'object',
+            patternProperties: {
+              '.+': { type: 'number' }
+            }
+          }
+        }
+      }
     }
   }
 }
