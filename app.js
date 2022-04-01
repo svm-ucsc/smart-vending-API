b8e25f64fcf6574ecb3196cc4067887692d2e9e7
brngit-code101 <brnlin@ucsc.edu>
2022-04-01 12:26:55 -0700

    Revert changes to mclient-test, I think app.js bug was with forgetting to remove parent

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ module.exports = async function (fastify, opts) {
     }
   })
 
-  fastify.register(require('fastify-mqtt'), parent => ({
+  fastify.register(require('fastify-mqtt'), {
     host: 'http://ec2-3-87-77-241.compute-1.amazonaws.com:1884',
     username: 'lenatest',
     password: 'password'
-  }))
+  })
 
   fastify.register(require('./plugins/fastify-mclient.js'))
 
--- test/plugins/fastify-mclient.test.js
+++ test/plugins/fastify-mclient.test.js
@@ -8,7 +8,7 @@ test('support works standalone', async (t) => {
   const fastify = Fastify()
 
   fastify.register(require('fastify-env'), {
-    dotenv: false,
+    dotenv: true,
     schema: {
       type: 'object',
       required: [ 'MQTT_HOST', 'MQTT_USERNAME', 'MQTT_PASSWORD' ],
@@ -26,11 +26,11 @@ test('support works standalone', async (t) => {
     }
   })
 
-  fastify.register(require('fastify-mqtt'), {
-    host: 'http://ec2-3-87-77-241.compute-1.amazonaws.com:1884',
-    username: 'lenatest',
-    password: 'password'
-  })
+  fastify.register(require('fastify-mqtt'), parent => ({
+    host: parent.config.MQTT_HOST,
+    username: parent.config.MQTT_USERNAME,
+    password: parent.config.MQTT_PASSWORD
+  }))
 
   fastify.register(customMqtt, {
     host: 'http://ec2-3-87-77-241.compute-1.amazonaws.com:1884',
