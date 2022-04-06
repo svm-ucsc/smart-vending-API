7bcf1134bc44b61b2d56208c827846e58c770310
brngit-code101 <brnlin@ucsc.edu>
2022-04-06 14:35:30 -0700

    Finished writing index.js for change machine status route

--- routes/admin/test_status/index.js
+++ routes/admin/test_status/index.js
@@ -9,9 +9,10 @@ const schema = {
     summary: 'DynamoDB admin functions',
     body: {
         type: 'object',
-        required: ['machine_id'],
+        required: ['machine_id', 'status'],
         properties: {
-            machine_id: {type: 'string'}
+            machine_id: {type: 'string'},
+            status: {type: 'string'}
         }
     },
     response: {
@@ -31,28 +32,30 @@ const schema = {
 }
 
 module.exports = async function (fastify, opts) {
-    fastify.post('/', {schema}, async function (request, reply)) {
+    fastify.post('/', {schema}, async function (request, reply) {
         const machineId = request.body['machine_id']
-    }
+        const newStatus = request.body['status']
 
-    const statusCheckParams = {
-        TableName: 'inventory',
-        Key: {
-            machine_id: machineId
-        },
-        AttributesToGet: ['status']
-    }
-    const statusCheckResponse = await dynamo.get(statusCheckParams)
-    console.log(statusCheckResponse.status)
-    if (statusCheckResponse.status == null) {
-        return reply.code(400).send({
-            reason: 'Machine Status field does not exist'
-        })
-    }
+        const statusCheckParams = {
+            TableName: 'inventory',
+            Key: {
+                machine_id: machineId
+            },
+            AttributesToGet: ['status']
+        }
+        
+        const statusCheckResponse = await dynamo.get(statusCheckParams)
+        
+        if (statusCheckResponse.status == null) {
+            return reply.code(400).send({
+                reason: 'Machine Status field does not exist'
+            })
+        }
 
-    // SET MACHINE STATUS IN DB
-    await setMachineStatusFromDB(machineId, , this.dynamo)
-    statusCheckResponse = await dynamo.get(statusCheckParams)
-    console.log(statusCheckResponse.status)
-    return reply.code(200).send(statusCheckResponse.status);
+        // SET MACHINE STATUS IN DB
+        await setMachineStatusFromDB(machineId, newStatus, this.dynamo)
+        statusCheckResponse = await dynamo.get(statusCheckParams)
+        console.log(statusCheckResponse.status)
+        return reply.code(200).send(statusCheckResponse.status);
+    })
 }
