dc0f90080b2f75140b2d860f88ee45c12095bce4
brngit-code101 <brnlin@ucsc.edu>
2022-04-14 13:49:23 -0700

    fixed npm lint errors

--- routes/admin/admin_status/index.js
+++ routes/admin/admin_status/index.js
@@ -1,69 +1,67 @@
 'use strict'
 const { setMachineStatusFromDB } = require('../util')
-const { v4 } = require('uuid')
-const order = require('../../order')
 
 const schema = {
-    description: 'DynamoDB admin functions. Change status of a machine',
-    tags: ['routes'],
-    summary: 'DynamoDB admin functions',
-    body: {
-        type: 'object',
-        required: ['machine_id', 'status'],
-        properties: {
-            machine_id: {type: 'string'},
-            status: {type: 'string'}
-        }
-    },
-    response: {
-        200: {
-            description: 'Updating the machine status success.',
-            type: 'object',
-            properties: {
-                machine_status: { type: 'string' }
-            }
-        },
-        400: {
-            description: 'Updating the machine status failed, Failure reason returned as string.',
-            type: 'object',
-            additionalProperties: true
-        }
+  description: 'DynamoDB admin functions. Change status of a machine',
+  tags: ['routes'],
+  summary: 'DynamoDB admin functions',
+  body: {
+    type: 'object',
+    required: ['machine_id', 'status'],
+    properties: {
+      machine_id: {type: 'string'},
+      status: {type: 'string'}
     }
+  },
+  response: {
+    200: {
+      description: 'Updating the machine status success.',
+      type: 'object',
+      properties: {
+        machine_status: { type: 'string' }
+      }
+    },
+    400: {
+      description: 'Updating the machine status failed, Failure reason returned as string.',
+      type: 'object',
+      additionalProperties: true
+    }
+  }
 }
 
 module.exports = async function (fastify, opts) {
-    fastify.post('/', {schema}, async function (request, reply) {
-        const machineId = request.body['machine_id']
-        const newStatus = request.body['status']
+  fastify.post('/', {schema}, async function (request, reply) {
+    const machineId = request.body['machine_id']
+    const newStatus = request.body['status']
 
-        const statusCheckParams = {
-            TableName: 'inventory',
-            Key: {
-                machine_id: machineId
-            },
-            AttributesToGet: ['status']
-        }
-        
-        const statusCheckResponse = await dynamo.get(statusCheckParams)
-        
-        if (!statusCheckResponse.Item.status) {
-            return reply.code(400).send({
-                reason: 'Machine Status field does not exist'
-            })
-        }
+    const statusCheckParams = {
+      TableName: 'inventory',
+      Key: {
+        machine_id: machineId
+      },
+      AttributesToGet: ['status']
+    }
 
-        // SET MACHINE STATUS IN DB
-        await setMachineStatusFromDB(machineId, newStatus, this.dynamo)
-        statusCheckResponse = await dynamo.get(statusCheckParams)
-        if (statusCheckResponse.Item.status == newStatus) {
-            console.log(statusCheckResponse.status)
-            return reply.code(200).send(statusCheckResponse.status)
-        } else {
-            return reply.code(400).send({
-                reason: 'Status returned does not match expected',
-                statusSent: newStatus,
-                statusReturned: statusCheckResponse.Item.status
-            })
-        }
-    })
+    let statusCheckResponse = await this.dynamo.get(statusCheckParams)
+
+    if (!statusCheckResponse.Item.status) {
+      return reply.code(400).send({
+        reason: 'Machine Status field does not exist'
+      })
+    }
+
+    // SET MACHINE STATUS IN DB
+    await setMachineStatusFromDB(machineId, newStatus, this.dynamo)
+    statusCheckResponse = await this.dynamo.get(statusCheckParams)
+    if (statusCheckResponse.Item.status === newStatus) {
+      console.log(statusCheckResponse.status)
+      return reply.code(200).send(statusCheckResponse.status)
+    } else {
+      return reply.code(400).send({
+        reason: 'Status returned does not match expected',
+        statusSent: newStatus,
+        statusReturned: statusCheckResponse.Item.status
+      })
+    }
+  })
 }
--- routes/admin/admin_stock/index.js
+++ routes/admin/admin_stock/index.js
@@ -1,57 +1,56 @@
 'use strict'
 const { setItemStockFromDB } = require('../util')
-const { v4 } = require('uuid')
 
 const schema = {
-    description: 'DynamoDB admin functions. Change stock of item in a machine',
-    tags: ['routes'],
-    summary: 'DynamoDB admin functions',
-    body: {
-        type: 'object',
-        required: ['machine_id', 'item_stock'],
-        properties: {
-            machine_id: { type: 'string' },
-            item_stock: { type: 'object' }
-        }
-    },
-    response: {
-        200: {
-            description: 'Updating the item stock of a machine, succeeded.',
-            type: 'object'
-        },
-        400: {
-            description: 'Updating the item of a machine, failed.',
-            type: 'object',
-            additionalProperties: true
-        }
+  description: 'DynamoDB admin functions. Change stock of item in a machine',
+  tags: ['routes'],
+  summary: 'DynamoDB admin functions',
+  body: {
+    type: 'object',
+    required: ['machine_id', 'item_stock'],
+    properties: {
+      machine_id: { type: 'string' },
+      item_stock: { type: 'object' }
     }
+  },
+  response: {
+    200: {
+      description: 'Updating the item stock of a machine, succeeded.',
+      type: 'object'
+    },
+    400: {
+      description: 'Updating the item of a machine, failed.',
+      type: 'object',
+      additionalProperties: true
+    }
+  }
 }
 
 module.exports = async function (fastify, opts) {
-    fastify.post('/', schema, async function (request, reply) {
-        const machineId = request.body['machine_id']
-        const itemStock = request.body['item_stock']
+  fastify.post('/', schema, async function (request, reply) {
+    const machineId = request.body['machine_id']
+    const itemStock = request.body['item_stock']
 
-        const stockCheckParams = {
-            TableName: 'inventory',
-            Key: {
-                machine_id: machineId
-            },
-            AttributesToGet: ['item_stock']
-        }
+    const stockCheckParams = {
+      TableName: 'inventory',
+      Key: {
+        machine_id: machineId
+      },
+      AttributesToGet: ['item_stock']
+    }
 
-        const stockCheckResponse = await dynamo.get(stockCheckParams)
-        
-        if (!stockCheckResponse.Item.status) {
-            return reply.code(400).send({
-                reason: 'Item stock field does not exist'
-            })
-        }
+    let stockCheckResponse = await this.dynamo.get(stockCheckParams)
 
-        // SET ITEM STOCK IN DB
-        await setItemStockFromDB(machineId, itemStock, this.dynamo)
-        stockCheckResponse = await dynamo.get(stockCheckParams)
-        console.log(stockCheckResponse.status)
-        return reply.code(200).send(stockCheckResponse.status);
-    })
+    if (!stockCheckResponse.Item.status) {
+      return reply.code(400).send({
+        reason: 'Item stock field does not exist'
+      })
+    }
+
+    // SET ITEM STOCK IN DB
+    await setItemStockFromDB(machineId, itemStock, this.dynamo)
+    stockCheckResponse = await this.dynamo.get(stockCheckParams)
+    console.log(stockCheckResponse.status)
+    return reply.code(200).send(stockCheckResponse.status)
+  })
 }
